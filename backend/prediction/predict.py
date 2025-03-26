af0ed3a7cdbef512ae5b4774da8b48f6635a03b0
Aitijhya Modak <aitijhyasps1@gmail.com>
2025-03-26 18:01:30 +0530

    Fix wrong prediction error

--- backend/prediction/predict.py
+++ backend/prediction/predict.py
@@ -5,21 +5,15 @@ import numpy as np
 import pickle
 import warnings
 warnings.simplefilter('ignore')
-import re
 
 with open("./model/symptoms.pkl", "rb") as f:
     symptom_columns = pickle.load(f)
 
-def preprocess_text(user_text):
-    return re.split(r',\s*|\s+', user_text.strip())
-
 def match_synonyms(user_text, model, indices, symptom_embeddings, threshold=0.7):
     user_embedding = model.encode(user_text)
-    extracted_symptoms = preprocess_text(user_text)
-    print(extracted_symptoms)
 
     matched_symptoms = []
-    for j in extracted_symptoms:
+    for j in user_text.split(','):
         user_embedding = model.encode(j)
         for i, symptom in enumerate(symptom_columns):
             similarity = util.cos_sim(user_embedding, symptom_embeddings[i]).item()
